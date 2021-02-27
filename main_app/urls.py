c7f5d3754d91eded244b4b7b64c1f5b63e18dc12
Nicholas Taveras III <taveras123n@gmail.com>
2021-02-27 14:06:41 -0500

    adds paths for dashboard and update account

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard_index, name='dashboard_index'),
     path('dashboard/accounts/', views.AccountCreate.as_view(), name='account_create'),
     path('dashboard/accounts/<int:account_id>/', views.account_dashboard, name='account_dashboard'),
     path('topics/', views.topics_index, name='topics_index'),
