7ce5813a9fe5a7509bfcc76826a72051005086c9
Nicholas Taveras <taveras123n@gmail.com>
2023-09-23 17:11:26 -0400

    Config Deployment

--- projectdjango/settings.py
+++ projectdjango/settings.py
@@ -35,14 +35,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app',
-    'mathfilters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mathfilters',
+    'main_app',
 ]
 
 MIDDLEWARE = [
